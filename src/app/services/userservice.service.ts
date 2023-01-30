992f8901cd6df2ffc05cb1fd903ec738a5624296
fimovol <fimovolpe@gmail.com>
2023-01-30 09:23:32 -0500

    feat: add service and url users reto 26

--- src/app/services/userservice.service.ts
+++ src/app/services/userservice.service.ts
@@ -2,6 +2,7 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { environment } from 'src/environments/environment';
+import { Reto26, reto26array } from '../models/reto26';
 import { Users } from '../models/users';
 
 @Injectable({
@@ -45,5 +46,9 @@ export class UserserviceService {
   postActualizar(usuario: Users): Observable<Users>{
     let apiUser = environment.apiURL + 'actualizar/' ;
     return this.http.post<Users>(apiUser, usuario);
+  }
+  getReto26All():Observable<reto26array>{
+    let apiUser = environment.rutareto26 + 'users/';
+    return this.http.get<reto26array>(apiUser);
   }
 }
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,5 @@
 export const environment = {
     production: false,
-    apiURL: "http://localhost/g2apirest/"
+    apiURL: "http://localhost/g2apirest/",
+    rutareto26: "https://jsonplaceholder.typicode.com/"
 }
