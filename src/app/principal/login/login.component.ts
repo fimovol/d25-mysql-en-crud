e6a804d63ccbbab0e2fa6d9c9db7632da16c364f
fimovol <fimovolpe@gmail.com>
2023-01-30 10:13:10 -0500

    feat(login): asincrona26 works

--- src/app/principal/login/login.component.ts
+++ src/app/principal/login/login.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
+import { reto26array } from 'src/app/models/reto26';
 import { AutenticacionService } from 'src/app/services/autenticacion.service';
+import { UserserviceService } from 'src/app/services/userservice.service';
 
 @Component({
   selector: 'app-login',
@@ -9,7 +11,9 @@ import { AutenticacionService } from 'src/app/services/autenticacion.service';
 })
 export class LoginComponent {
 
-  constructor(private autenticacion: AutenticacionService, private router: Router){}
+  constructor(private autenticacion: AutenticacionService,
+     private router: Router,
+     private userService: UserserviceService){}
   
   redireccion = '';
   usurio = ""
@@ -18,7 +22,30 @@ export class LoginComponent {
     this.autenticacion.login();
     this.redireccion = this.autenticacion.urlUsuarioIntentaAcceder;
     this.autenticacion.urlUsuarioIntentaAcceder = '';
-    this.router.navigate([this.redireccion]);
+    this.router.navigate(["reto26"]);
+  }
+  onSubmit(){
+    this.listado.some(elemento => {
+      let user = elemento.username
+      let pass = elemento.email
+      if(user === this.usurio && pass === this.password){
+        this.login()
+        console.log("si existe")
+        return true
+      }else{
+        console.log("NO existe")
+        return false
+      }
+    })
+  }
+  listado: reto26array = []
+  ngOnInit() {
+    console.log("asdasd")
+    this.userService.getReto26All().subscribe({
+      next: (userAll:reto26array) => {
+        this.listado = userAll
+      }
+    })
   }
 
 }
